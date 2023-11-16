dfd2d0d13e87674296ef8cc316d9fc15a5f0a4a5
DVNO <dvno@live.se>
2023-11-16 16:10:23 +0100

    OwnerReservationClient done continue with styling on mobile devices

--- app/reservations/OwnerReservationClient.tsx
+++ app/reservations/OwnerReservationClient.tsx
@@ -10,6 +10,7 @@ import { SafeReservation, SafeUser } from '../types'
 import Container from '../components/Container'
 import HeadingAccount from '../components/HeadingAccount'
 import AccountCard from '../components/account/AccountCard'
+import { isAfter, parseISO, startOfToday } from 'date-fns'
 
 interface OwnerReservationClientProps {
   reservations: SafeReservation[]
@@ -43,6 +44,22 @@ const OwnerReservationClient: React.FC<OwnerReservationClientProps> = ({
     [router]
   )
 
+  const isReservationOld = (startDate: string) => {
+    return !isAfter(parseISO(startDate), startOfToday())
+  }
+
+  const sortedReservations = [...reservations].sort(
+    (a, b) => parseISO(b.startDate).getTime() - parseISO(a.startDate).getTime()
+  )
+
+  const newReservations = sortedReservations.filter(
+    (reservation) => !isReservationOld(reservation.startDate)
+  )
+
+  const oldReservations = sortedReservations.filter((reservation) =>
+    isReservationOld(reservation.startDate)
+  )
+
   return (
     <>
       <Container>
@@ -55,12 +72,12 @@ const OwnerReservationClient: React.FC<OwnerReservationClientProps> = ({
       </Container>
 
       <div className="flex flex-row py-3 xl:px-20 lg:px-8 md:px-10 sm:px-5 px-5 bg-neutral-700">
-        <span className="px-2 text-white text-2xl font-bold">Bookings</span>
+        <span className="px-2 text-white text-2xl font-bold">New Reservations</span>
       </div>
 
       <Container>
         <div>
-          {reservations.map((reservation: any) => (
+          {newReservations.map((reservation: any) => (
             <AccountCard
               key={reservation.id}
               data={reservation.listing}
@@ -76,8 +93,26 @@ const OwnerReservationClient: React.FC<OwnerReservationClientProps> = ({
       </Container>
 
       <div className="flex flex-row py-3 xl:px-20 lg:px-8 md:px-10 sm:px-5 px-5 bg-neutral-700">
-        <span className="px-2 text-white text-2xl font-bold">Old Reservations</span>
+        <span className="px-2 text-white text-2xl font-bold">
+          Old Reservations
+        </span>
       </div>
+        <Container>
+          <div>
+            {oldReservations.map((reservation: any) => (
+              <AccountCard
+                key={reservation.id}
+                data={reservation.listing}
+                reservation={reservation}
+                actionId={reservation.id}
+                onAction={onCancel}
+                disabled={deletingId === reservation.id}
+                actionLabel="Cancel guest reservation"
+                currentUser={currentUser}
+              />
+            ))}
+          </div>
+        </Container>
     </>
   )
 }
