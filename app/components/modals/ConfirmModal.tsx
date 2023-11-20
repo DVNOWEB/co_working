d27ff241e9d354ca15a6c300b8df43e58bef37a7
DVNO <dvno@live.se>
2023-11-20 15:47:14 +0100

    small changes

--- app/components/modals/ConfirmModal.tsx
+++ app/components/modals/ConfirmModal.tsx
@@ -3,7 +3,6 @@
 import useConfirmModal from '@/app/hooks/useConfirmModal'
 import { useEffect, useState, useCallback } from 'react'
 
-
 interface ConfirmModalProps {
   isOpen?: boolean
   onClose?: () => void
@@ -17,9 +16,9 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({
   disabled,
 }) => {
   const [showModal, setShowModal] = useState(ModalIsOpen)
-  const [smallScreen, setSmallScreen] = useState(false)
   const modalIsOpen = useConfirmModal((state) => state.isOpen)
   const modalOnClose = useConfirmModal((state) => state.onClose)
+  const [smallScreen, setSmallScreen] = useState(false)
 
   useEffect(() => {
     setShowModal(modalIsOpen)
@@ -47,29 +46,23 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({
   }
 
   return (
-    <div className="fixed inset-0 z-50 overflow-y-auto">
-      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 transition-opacity">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-
-        {/* Centering Trick */}
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen">
-          &#8203;
-        </span>
-
-        {/* Adjust width, max-width, and border-radius for modal */}
-        <div className="inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-xl md:max-w-6xl sm:w-full lg:w-3/4 xl:w-1/2 custom-border-radius">
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start mx-10 my-10">
-              {' '}
-              {/* Adjusted margins */}
-              <div className="text-center mx-auto py-4 md:py-24 px-4 md:px-24">
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-40 outline-none focus:outline-none  bg-gray-700  bg-opacity-70 style={ smallScreen ? 'top-16' : ''}">
+      <div className="relative w-full md:w-4/6 my-6 mx-auto h-full md:h-3/6 lg:h-auto">
+        <div
+          className={` translate duration-300 h-full
+          ${showModal ? 'translate-y-0' : 'translate-y-full'}
+          ${showModal ? 'opacity-100' : 'opacity-0'}
+        `}>
+          {/* Modal Content */}
+          <div
+            className=" translate h-full lg:h-auto md:h-auto border-0 shadow-lg relative flex flex-col w-full  bg-white outline-none focus:outline-none custom-border-radius
+            ">
+            <div className="flex items-center justify-center h-full">
+              <div className="px-4 py-8 sm:p-6 sm:pb-4 items-center">
                 <h3
-                  className="text-4xl leading-10 font-bold text-gray-700 mb-4"
-                  id="modal-title"
-                  style={{ letterSpacing: '0.05em', lineHeight: '1.5' }}>
-                  Thank you for your{' '}
+                  className="text-6xl text-center font-bold text-gray-700 mb-4"
+                  id="modal-title">
+                  Thank you for <br /> your{' '}
                   <span className="text-yellow-400">booking!</span>
                 </h3>
                 {/* Additional content can go here */}
@@ -80,6 +73,6 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({
       </div>
     </div>
   )
-};
+}
 
 export default ConfirmModal
